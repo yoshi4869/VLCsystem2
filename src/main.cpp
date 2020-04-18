3fd70b58443c238b404a5fbfe47450245d553d29
yoshi4869 <yoshihito4869@gmail.com>
2020-04-19 00:38:04 +0900

    1つのLEDで出力するようにシンプルにした

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,11 @@
 
 #define H HIGH
 #define L LOW
-#define INTERVAL 20 //点灯間隔
+#define INTERVAL 3000 //点灯間隔
 
 const int LEDpin = 8;
-const byte interruptPin = 2;//2番ピンの割り込み0を使う
-volatile int flag= LOW;
 //{0 1}={00100 01110}の２値で明暗を表現
 
-void turnoff() {
-    flag= !flag;
-}
 
 void pwm0(int led,float t){//00100=LLHLL
   digitalWrite(led,L);
@@ -38,53 +33,24 @@ void pwm1(int led,float t){//01110=LHHHL
   delayMicroseconds(t);
 }
 
-//データ部分の抽出に必要
-void head(int led,float t){//11111=HHHHH
-  digitalWrite(led,H);
-  delayMicroseconds(t);
-  digitalWrite(led,H);
-  delayMicroseconds(t);
-  digitalWrite(led,H);
-  delayMicroseconds(t);
-  digitalWrite(led,H);
-  delayMicroseconds(t);
-  digitalWrite(led,H);
-  delayMicroseconds(t);
-}
 
 //pwm0とpwm1でデータを作る
 void data(int led, float t){
-  //head(led,t);
-  pwm1(led,t);
   pwm0(led,t);
   pwm0(led,t);
-  pwm1(led,t);
   pwm0(led,t);
-  //head(led,t);
+  pwm0(led,t);
+  pwm0(led,t);
 }
-//6つのLEDで出力する
+//1つのLEDで出力する
 void outputLED() {
-  for(int i = LEDpin;i < LEDpin + 6;i++){
-  data(i, INTERVAL);
-  }
-}
-//6つのLEDの出力を止める
-void turnOff() {
-  for(int i = LEDpin;i < LEDpin + 6;i++){
-  digitalWrite(i, L);
-  }
+  data(LEDpin, INTERVAL);
 }
 
 void setup() {
-  //割り込みSWの設定
-  pinMode(interruptPin, INPUT_PULLUP);
-  pinMode(LEDpin, OUTPUT);
-  //attachInterrupt(digitalPinToInterrupt(interruptPin), outputLED, LOW);
   //LEDピンの設定
   pinMode(LEDpin, OUTPUT);
-  for(int i = LEDpin ;i < LEDpin + 6;i++){
-  pinMode(i,OUTPUT);
-  }
+  pinMode(LEDpin,OUTPUT);
 }
 void loop() {
   outputLED();
